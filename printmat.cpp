5c4cdbfaca1b2d1aa7fb33d74889c67c7e696f70
MohitAhirwar <66860602+MohitAhirwar@users.noreply.github.com>
2020-10-25 22:17:19 +0530

    Add files via upload

---
+++ printmat.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int A[3][3];
+for(int i=0;i<3;i++)
+{ cout<<endl;
+    for(int j=0;j<3;j++)
+    cin>>A[i][j];
+}
+
+for(int i=0;i<3;i++)
+{ cout<<endl;
+    for(int j=0;j<3;j++)
+    cout<<A[i][j];
+}
+
+}
